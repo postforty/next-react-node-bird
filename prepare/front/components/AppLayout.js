ca664395170597beb153102e6395e40dd80185b2
postforty <75767073+postforty@users.noreply.github.com>
2023-08-14 22:35:39 +0900

    [ADD] 반응형 그리드 사용하기

--- prepare/front/components/AppLayout.js
+++ prepare/front/components/AppLayout.js
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Link from "next/link";
-import { Menu } from "antd";
+import { Menu, Input, Row, Col } from "antd";
 
 const AppLayout = ({ children }) => {
   return (
@@ -14,10 +14,31 @@ const AppLayout = ({ children }) => {
           <Link href="/profile">프로필</Link>
         </Menu.Item>
         <Menu.Item>
+          <Input.Search enterButton style={{ verticalAlign: "middle" }} />
+        </Menu.Item>
+        <Menu.Item>
           <Link href="/signup">회원가입</Link>
         </Menu.Item>
       </Menu>
-      <div>{children}</div>
+      {/* 거터: 컬럼 사이의 간격 */}
+      <Row gutter={8}>
+        <Col xs={24} md={6}>
+          왼쪽
+        </Col>
+        <Col xs={24} md={12}>
+          {children}
+        </Col>
+        <Col xs={24} md={6}>
+          {/* rel="noreferrer noopenr" : 보안 위협 방지 */}
+          <a
+            href="https://postforty.tistory.com"
+            target="_blank"
+            rel="noreferrer noopenr"
+          >
+            Go to Blog!
+          </a>
+        </Col>
+      </Row>
     </div>
   );
 };
@@ -29,3 +50,5 @@ AppLayout.propTypes = {
 };
 
 export default AppLayout;
+
+// https://www.inflearn.com/course/lecture?courseSlug=%EB%85%B8%EB%93%9C%EB%B2%84%EB%93%9C-%EB%A6%AC%EC%95%A1%ED%8A%B8-%EB%A6%AC%EB%89%B4%EC%96%BC&unitId=48793
--- prepare/front/pages/_app.js
+++ prepare/front/pages/_app.js
@@ -10,7 +10,7 @@ const NodeBird = ({ Component }) => {
         <meta charSet="utf-8" />
         <title>NodeBird</title>
       </Head>
-      <div>공통 메뉴</div>
+      {/* <div>공통 메뉴</div> */}
       <Component />
     </>
   );
