ccd4444932e31f86fd3aadac731f8f89e2bee8e5
postforty <75767073+postforty@users.noreply.github.com>
2023-08-23 22:44:01 +0900

    [ADD] 해시태그

--- prepare/front/components/PostCard.js
+++ prepare/front/components/PostCard.js
@@ -10,6 +10,7 @@ import {
 } from "@ant-design/icons";
 import PostImages from "./PostImages";
 import CommentForm from "./CommentForm";
+import PostCardContent from "./PostCardContent";
 
 const PostCard = ({ post }) => {
   const [liked, setLiked] = useState(false);
@@ -67,7 +68,7 @@ const PostCard = ({ post }) => {
         <Card.Meta
           avatar={<Avatar>{post.User.nickname[0]}</Avatar>}
           title={post.User.nickname}
-          description={post.content}
+          description={<PostCardContent postData={post.content} />}
         />
       </Card>
       {commentFormOpened && (
